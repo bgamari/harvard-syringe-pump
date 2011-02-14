a073e006852b02e5e5c55f1bf0c5cafdb510816a
Ben Gamari <bgamari.foss@gmail.com>
2011-02-14 10:32:20 -0500

    Move sources into harvardpump package

--- setup.py
+++ setup.py
@@ -1,14 +1,13 @@
 from distutils.core import setup
 
-setup(name='harvard_syringe_pump',
-      #version='1.0',
-      description="Python interface to Harvard Apparatus OEM syringe pump modules",
-      author="Ben Gamari",
-      author_email="bgamari@physics.umass.edu",
-      url="http://goldnerlab.physics.umass.edu/wiki",
-      py_modules=['pump'],
-      scripts=['pump_ui.py'],
-      data_files=[
-              ('share/harvard_syringe_pump', ['pump_ui.glade']),
-      ],
-     )
+setup(name = 'harvard_syringe_pump',
+      version = '1.0',
+      description = "Python interface to Harvard Apparatus OEM syringe pump modules",
+      author = "Ben Gamari",
+      author_email = "bgamari@physics.umass.edu",
+      url = "http://goldnerlab.physics.umass.edu/wiki",
+      packages = ['harvardpump'],
+      scripts = ['harvardpump_ui'],
+      package_data = {'harvardpump': ['ui.glade']},
+      license = 'GPLv3',
+)
